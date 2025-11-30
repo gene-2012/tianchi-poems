bdeafe10d04808dc5b5d610d0ed192f6db9a1bb0
gene-2012 <gene201248@126.com>
2025-11-30 13:44:45 +0800

    New Prompt

--- baseline.py
+++ baseline.py
@@ -17,55 +17,15 @@ client = OpenAI(
 )
 model = 'Qwen/Qwen3-8B'
 print(model)
+with open("prompts/version1.txt", "r", encoding="utf-8") as f:
+    prompt = f.read()
 def get_response(data):
-    prompt = f"""
-        你是一个古诗词专家，现在有一些古诗词需要你的帮助。
-        我会给你提供一个 JSON 数据，格式如下：
-        - **"index"**：古诗词的序号  
-        - **"title"**：古诗词的标题  
-        - **"author"**：古诗词的作者  
-        - **"content"**：古诗词的内容  
-        - **"qa_words"**：古诗词中需要翻译的词语  
-        - **"qa_sents"**：古诗词中需要提供白话文译文的句子  
-        - **"choose"**：一个包含多个选项的字典，每个选项代表该诗词可能表达的情感  
-
-        这是我的数据：{data}
-
-        ### 你的任务：
-        请你根据提供的数据，生成如下 JSON 格式的结果：
-        - **"ans_qa_words"**：对 "qa_words" 词语的含义进行解释  
-        - **"ans_qa_sents"**：对 "qa_sents" 句子提供白话文译文  
-        - **"choose_id"**：从 "choose" 选项中选择最符合该古诗词情感的标号，仅输出对应的字母  
-
-        ### **json输出格式示例：**
-
-        {{
-            "idx": 古诗词的序号,
-            "ans_qa_words": {{
-                "词语1": "词语1的含义",
-                "词语2": "词语2的含义",
-                "词语3": "词语3的含义"
-            }},
-            "ans_qa_sents": {{
-                "句子1": "句子1的白话文翻译",
-                "句子2": "句子2的白话文翻译"
-            }},
-            "choose_id": ""
-        }}
-
-        ### **注意事项：**
-        1. **仅返回 JSON 结果，不需要额外解释或输出其他内容。**  
-        2. **请确保 "choose_id" 只输出选项的字母，不要附加其他文本。**  
-        3. **请保持输出格式整洁，符合 JSON 规范。**
-        4.**不要给出任何注释和注意，只需要给出答案，不需要给出其他无关内容**
-        5.**请确保输出格式整洁，符合 JSON 规范。**
-        6.**必须用中文解答。**
-    """
+    global prompt
     for _ in range(3):  # 三次重传机制
         try:
             response = client.chat.completions.create(
                 model=model,
-                messages=[{"role": "user", "content": prompt}],
+                messages=[{"role": "user", "content": prompt % data}],
                 stream=False
             )
             # print(response)
